939df190a2de486333a087f5aaf33a9446f1b463
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-02-26 10:46:06 +0000

    Premier test du lien de l'API

--- src/Api/recipeProvider.js
+++ src/Api/recipeProvider.js
@@ -1,11 +1,12 @@
 async function ApiRecuperation() {
 
 try{
-    
-
-
+  const respnose= await fetch('https://dummyjson.com/recipes');
+  const date= respnose.json();  
+   console.log(data);
 }
 catch(Erreur){
+    alert("plat introuvable");
 
 }
     
